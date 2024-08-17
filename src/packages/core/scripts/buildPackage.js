860dcd945bd8b2579bfee5280b6d29489c872d96
jorgedanisc <jorgedanisc.io@gmail.com>
2024-08-17 23:50:43 +0100

    sass warnings finally hidden

--- src/packages/core/scripts/buildPackage.js
+++ src/packages/core/scripts/buildPackage.js
@@ -1,41 +1,6 @@
 const { build } = require("esbuild");
 const { sassPlugin } = require("esbuild-sass-plugin");
 const { externalGlobalPlugin } = require("esbuild-plugin-external-global");
-// Will be used later for treeshaking
-//const fs = require("fs");
-// const path = require("path");
-
-// function getFiles(dir, files = []) {
-//   const fileList = fs.readdirSync(dir);
-//   for (const file of fileList) {
-//     const name = `${dir}/${file}`;
-//     if (
-//       name.includes("node_modules") ||
-//       name.includes("config") ||
-//       name.includes("package.json") ||
-//       name.includes("main.js") ||
-//       name.includes("index-node.ts") ||
-//       name.endsWith(".d.ts")
-//     ) {
-//       continue;
-//     }
-
-//     if (fs.statSync(name).isDirectory()) {
-//       getFiles(name, files);
-//     } else if (
-//       !(
-//         name.match(/\.(sa|sc|c)ss$/) ||
-//         name.match(/\.(woff|woff2|eot|ttf|otf)$/) ||
-//         name.match(/locales\/[^/]+\.json$/)
-//       )
-//     ) {
-//       continue;
-//     } else {
-//       files.push(name);
-//     }
-//   }
-//   return files;
-// }
 
 const browserConfig = {
   entryPoints: ["index.tsx"],
@@ -56,6 +21,7 @@ const browserConfig = {
     ".ttf": "copy",
   },
 };
+
 const createESMBrowserBuild = async () => {
   // Development unminified build with source maps
   await build({
@@ -66,6 +32,7 @@ const createESMBrowserBuild = async () => {
     define: {
       "import.meta.env": JSON.stringify({ DEV: true }),
     },
+    logLevel: "error", // Set log level to error
   });
 
   // production minified build without sourcemaps
@@ -77,33 +44,19 @@ const createESMBrowserBuild = async () => {
     define: {
       "import.meta.env": JSON.stringify({ PROD: true }),
     },
+    logLevel: "error", // Set log level to error
   });
 };
-
-// const BASE_PATH = `${path.resolve(`${__dirname}/..`)}`;
-// const filesinExcalidrawPackage = [
-//   ...getFiles(`${BASE_PATH}/canvas`),
-//   `${BASE_PATH}/packages/utils/export.ts`,
-//   `${BASE_PATH}/packages/utils/bbox.ts`,
-//   ...getFiles(`${BASE_PATH}/public/fonts`),
-// ];
-
-// const filesToTransform = filesinExcalidrawPackage.filter((file) => {
-//   return !(
-//     file.includes("/__tests__/") ||
-//     file.includes(".test.") ||
-//     file.includes("/tests/") ||
-//     file.includes("example")
-//   );
-// });
 
 const rawConfig = {
   entryPoints: ["index.tsx"],
   bundle: true,
   format: "esm",
-  plugins: [sassPlugin({
-    quietDeps: true,
-  })],
+  plugins: [
+    sassPlugin({
+      quietDeps: true,
+    }),
+  ],
   loader: {
     ".woff2": "copy",
     ".ttf": "copy",
@@ -121,6 +74,7 @@ const createESMRawBuild = async () => {
     define: {
       "import.meta.env": JSON.stringify({ DEV: true }),
     },
+    logLevel: "error", // Set log level to error
   });
 
   // production minified build without sourcemaps
@@ -131,6 +85,7 @@ const createESMRawBuild = async () => {
     define: {
       "import.meta.env": JSON.stringify({ PROD: true }),
     },
+    logLevel: "error", // Set log level to error
   });
 };
 
