d77be9c5b82f4a6ac302b77411b09e8187b5dd98
jorgedanisc <jorgedanisc.io@gmail.com>
2024-09-28 15:08:03 +0100

    Granular fixes bug: 1 feat: 2

--- release.config.cjs
+++ release.config.cjs
@@ -1,25 +1,6 @@
 module.exports = {
-  branches: [
-    "main",
-    {
-      name: "next",
-      prerelease: true,
-    },
-  ],
-  plugins: [
-    [
-      "@semantic-release/commit-analyzer",
-      {
-        preset: "conventionalcommits",
-        releaseRules: [
-          { type: "bug", release: "patch" },
-          // Add other custom rules if needed
-        ],
-      },
-    ],
-    "@semantic-release/release-notes-generator",
-    "@semantic-release/changelog",
-    "@semantic-release/github",
-    "@semantic-release/npm",
-  ],
+    branches: ["main", {
+        name: "next",
+        prerelease: true
+    }],
 };
