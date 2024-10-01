2cc51fde7e17c4e2ea5eff0201ca95383e229823
jorgedanisc <jorgedanisc.io@gmail.com>
2024-10-01 15:06:48 +0100

    chore: building monorepo

---
+++ src/packages/duc/src/index.ts
@@ -0,0 +1,15 @@
+
+
+export { parseAppStateFromBinary } from "../../core/canvas/duc/duc-ts/parse/parseAppStateFromBinary";
+export { parseElementFromBinary } from "../../core/canvas/duc/duc-ts/parse/parseElementFromBinary";
+export { parseBinaryFilesFromBinary } from "../../core/canvas/duc/duc-ts/parse/parseBinaryFilesFromBinary";
+
+export { parseDucFlatBuffers } from "../../core/canvas/duc/duc-ts/parse/index";
+
+export { serializeAppState } from "../../core/canvas/duc/duc-ts/serialize/appStateSerialize";
+export { serializeDucElement } from "../../core/canvas/duc/duc-ts/serialize/ducElementSerialize";
+export { serializeBinaryFiles } from "../../core/canvas/duc/duc-ts/serialize/binaryFilesSerialize";
+
+export { serializeAsFlatBuffers } from "../../core/canvas/duc/duc-ts/serialize/index";
+
+export * from "../../core/canvas/duc/duc-ts/duc";
