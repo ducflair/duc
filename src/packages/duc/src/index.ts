03ff5a57ed8a4e3cf9ab31b7022b5937523fd9cd
jorgedanisc <jorgedanisc.io@gmail.com>
2024-10-01 15:33:26 +0100

    chore: duc fix

--- src/packages/duc/src/index.ts
+++ src/packages/duc/src/index.ts
@@ -1,15 +1,11 @@
 
 
-export { parseAppStateFromBinary } from "../../core/canvas/duc/duc-ts/parse/parseAppStateFromBinary";
-export { parseElementFromBinary } from "../../core/canvas/duc/duc-ts/parse/parseElementFromBinary";
-export { parseBinaryFilesFromBinary } from "../../core/canvas/duc/duc-ts/parse/parseBinaryFilesFromBinary";
+// export { parseAppStateFromBinary } from "../../core/canvas/duc/duc-ts/parse/parseAppStateFromBinary";
+// export { parseElementFromBinary } from "../../core/canvas/duc/duc-ts/parse/parseElementFromBinary";
+// export { parseBinaryFilesFromBinary } from "../../core/canvas/duc/duc-ts/parse/parseBinaryFilesFromBinary";
 
-export { parseDucFlatBuffers } from "../../core/canvas/duc/duc-ts/parse/index";
+export { parseDucFlatBuffers, parseAppStateFromBinary, parseElementFromBinary, parseBinaryFilesFromBinary } from "@ducflair/canvas";
 
-export { serializeAppState } from "../../core/canvas/duc/duc-ts/serialize/appStateSerialize";
-export { serializeDucElement } from "../../core/canvas/duc/duc-ts/serialize/ducElementSerialize";
-export { serializeBinaryFiles } from "../../core/canvas/duc/duc-ts/serialize/binaryFilesSerialize";
-
-export { serializeAsFlatBuffers } from "../../core/canvas/duc/duc-ts/serialize/index";
+export { serializeAsFlatBuffers, serializeAppState, serializeDucElement, serializeBinaryFiles } from "@ducflair/canvas"
 
 export * from "../../core/canvas/duc/duc-ts/duc";
