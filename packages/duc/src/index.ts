8282fe54c629415cbe1ae64e7b6e644026048bff
jorgedanisc <jorgedanisc.io@gmail.com>
2024-10-01 17:10:05 +0100

    feat: bump correction

--- packages/duc/src/index.ts
+++ packages/duc/src/index.ts
@@ -1,5 +1,5 @@
 export { parseDucFlatBuffers, parseAppStateFromBinary, parseElementFromBinary, parseBinaryFilesFromBinary } from "@ducflair/canvas";
 
-export { serializeAsFlatBuffers, serializeAppState, serializeDucElement, serializeBinaryFiles } from "@ducflair/canvas"
+export { serializeAsFlatBuffers, serializeAppState, serializeDucElement, serializeBinaryFiles } from "@ducflair/canvas";
 
 export { DucBin } from "@ducflair/canvas"
