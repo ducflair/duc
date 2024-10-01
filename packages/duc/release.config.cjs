b776555c0400340d2fa317712ce85377ac0cd23e
jorgedanisc <jorgedanisc.io@gmail.com>
2024-10-01 17:40:59 +0100

    fix: bump release

--- packages/duc/release.config.cjs
+++ packages/duc/release.config.cjs
@@ -12,32 +12,13 @@ module.exports = {
       }
     ],
     "@semantic-release/github",
-    "@semantic-release/git"
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md"], // Exclude package.json
+        message: "chore(release): ${nextRelease.version} \n\n${nextRelease.notes}"
+      }
+    ]
   ],
-  tagFormat: "@ducflair/duc@${version}",
-  // Add this to ensure semantic-release uses the correct base version
-  verifyConditions: [
-    () => {
-      const npmVersion = require('child_process')
-        .execSync('npm view @ducflair/duc version')
-        .toString()
-        .trim();
-      process.env.PREVIOUS_VERSION = npmVersion;
-    },
-    '@semantic-release/npm',
-    '@semantic-release/github',
-  ],
-  analyzeCommits: {
-    preset: 'angular',
-    releaseRules: [
-      {type: 'feat', release: 'minor'},
-      {type: 'fix', release: 'patch'},
-      {type: 'docs', release: 'patch'},
-      {type: 'style', release: 'patch'},
-      {type: 'refactor', release: 'patch'},
-      {type: 'perf', release: 'patch'},
-      {type: 'test', release: 'patch'},
-      {type: 'chore', release: 'patch'},
-    ],
-  },
+  tagFormat: "@ducflair/duc@${version}"
 };
