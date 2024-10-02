2f36f8b8a721c4652bfd40fc1921a54ae83cb6f8
jorgedanisc <jorgedanisc.io@gmail.com>
2024-10-02 17:48:34 +0100

    fix: Update prepareCmd in duc-py release.config.cjs

--- packages/duc-rs/release.config.cjs
+++ packages/duc-rs/release.config.cjs
@@ -1,23 +1,23 @@
 module.exports = {
-  branches: ["main"],
+  branches: ["main", { name: "next", prerelease: true }],
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     [
       "@semantic-release/exec",
       {
-        "prepareCmd": "cargo set-version ${nextRelease.version} && cargo build --release",
-        "publishCmd": "cargo publish"
+        prepareCmd: "cargo install cargo-set-version && cargo set-version ${nextRelease.version} && cargo build --release",
+        publishCmd: "cargo publish"
       }
     ],
+    "@semantic-release/github",
     [
       "@semantic-release/git",
       {
-        "assets": ["Cargo.toml", "Cargo.lock"],
-        "message": "chore(release): ${nextRelease.version}\n\n${nextRelease.notes}"
+        assets: ["Cargo.toml"],
+        message: "chore(release): ${nextRelease.version}\n\n${nextRelease.notes}"
       }
-    ],
-    "@semantic-release/github"
+    ]
   ],
   tagFormat: "@ducflair/duc-rs@${version}"
 };
