bf1d1eea51f256ecbc4c7b51707c6274db6ea035
jorgedanisc <jorgedanisc.io@gmail.com>
2024-10-14 11:24:26 +0100

    Preparation for the python and rust availability releases

--- packages/duc-rs/src/lib.rs
+++ packages/duc-rs/src/lib.rs
@@ -1,3 +1,9 @@
+// #[path = "../core/canvas/duc/duc-rs/duc_generated.rs"]
+// include!("../../core/canvas/duc/duc-rs/duc_generated.rs");
+
+// mod duc_generated;
+// pub mod duc_generated { pub use duc_generated::*; }
+
 pub fn hello() -> String {
     "Hello from ducflair-duc Rust package!".to_string()
 }
