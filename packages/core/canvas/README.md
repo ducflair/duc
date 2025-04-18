9a8a7f0f366cca42430f9a6adc03dd4421f3f061
Jorge Soares <jorgedanisc13@gmail.com>
2025-04-18 23:20:10 +0100

    refactor: reshaping the duc repo

--- packages/core/canvas/README.md
+++
@@ -1,3 +0,0 @@
-# Ducflair Canvas
-
-This is a fork from the [Excalidraw](https://github.com/excalidraw/excalidraw) project.
---
+++ packages/duc-py/src/ducpy/serialize/__init__.py
@@ -0,0 +1,5 @@
+# Export serialize modules
+from .serialize_duc_element import *
+from .serialize_app_state import *
+from .serialize_binary_files import * 
+from .serialize_duc import *
