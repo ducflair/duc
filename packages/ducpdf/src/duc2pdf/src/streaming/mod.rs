f7ad40c9913216829f6b165a0a9ef87ec22a12a4
Jorge Soares <jorgedanisc13@gmail.com>
2025-09-06 14:25:30 +0100

    wip: prepare to develop duc2pdf

---
+++ packages/ducpdf/src/duc2pdf/src/streaming/mod.rs
@@ -0,0 +1,3 @@
+pub mod stream_setup;
+pub mod stream_resources;
+pub mod stream_elements;
--- packages/ducpdf/src/pdf2duc/index.ts
+++ packages/ducpdf/src/pdf2duc/index.ts
@@ -1,8 +1,7 @@
-import * as duc from '@ducflair/ducjs';
 import * as pdfjs from 'pdfjs-dist';
 
-export function convertPdfToDuc(pdfData: Uint8Array): duc.Duc {
+export function convertPdfToDuc(pdfData: Uint8Array): Uint8Array {
     console.log('Converting PDF to DUC...');
     // TODO: Implement conversion logic using pdf.js
-    return new duc.Duc();
+    return new Uint8Array();
 } 
