708800271cdd5f8c906c1713b134f45eb17d4252
Jorge Soares <jorgedanisc13@gmail.com>
2025-10-20 22:08:29 +0100

    fix(ducjs): deprecated v1 api

--- packages/ducjs/src/index.ts
+++ packages/ducjs/src/index.ts
@@ -4,6 +4,5 @@ export * from "./types";
 export * from "./utils";
 export * from "./serialize";
 export * from "./parse";
-export * from "./legacy";
 export * from "./restore";
 export * from "./technical";
