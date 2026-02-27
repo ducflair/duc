23b3af5dae0cbb6ee47b74ca0b4b015d9381742c
Jorge Soares <jorgedanisc13@gmail.com>
2026-02-27 19:21:46 +0000

    refactor: simplify analyzer and notes for some configs

--- packages/ducpdf/release.config.cjs
+++ packages/ducpdf/release.config.cjs
@@ -9,23 +9,8 @@ module.exports = {
           "packages/ducjs",
           "packages/ducrs"
         ],
-        // This is the one that controls the version bump (major/minor/patch)
-        analyzer: {
-          preset: "conventionalcommits",
-          parserOpts: {
-            // Temporarily set to a keyword nobody will use
-            noteKeywords: ["SKIP_BREAKING_DETECTION"],
-          },
-        },
-
-        // This controls release notes generation (so "Breaking Changes" sections, etc.)
-        // If you only change `analyzer`, you might still see BREAKING CHANGE notes in output notes.
-        notes: {
-          preset: "conventionalcommits",
-          parserOpts: {
-            noteKeywords: ["SKIP_BREAKING_DETECTION"],
-          },
-        },
+        analyzer: { preset: "conventionalcommits" },
+        notes: { preset: "conventionalcommits" },
       },
     ],
     [
